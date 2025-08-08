84587098f4b2423deb06d5dac2585a189f9fb5df
ron19907 <ronaldmweema@gmail.com>
2025-08-08 21:29:44 +0200

    Bootstrap MovieApp

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -1,12 +1,11 @@
-const Button = () => {
+import { ButtonProps } from "@/interfaces/index";
 
-    // This is a simple button component
-    
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
   return (
-	<button>
-	  Click me
-	</button>
-  );
-};
+    <button onClick={action} className="px-8 py-2 border-2 border-[#E2D609] rounded-full hover:bg-[#E2D609] hover:text-black transition-colors duration-300">
+      {title}
+    </button>
+  )
+}
 
 export default Button;
